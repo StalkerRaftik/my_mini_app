ea7d1af9e2248e777bbb24eda41c82eca1419d1e
WhileTrueDoEnd <altair-ivan@ya.ru>
2021-05-23 10:44:07 +1000

    2

--- src/panels/Home.js
+++ src/panels/Home.js
@@ -7,14 +7,14 @@ const Home = ({ id, go, fetchedUser }) => (
 	<Panel id={id}>
 		<PanelHeader>Example</PanelHeader>
 		{fetchedUser &&
-		<Group header={<Header mode="secondary">User Data Fetched with VK Bridge</Header>}>
-			<Cell
-				before={fetchedUser.photo_200 ? <Avatar src={fetchedUser.photo_200}/> : null}
-				description={fetchedUser.city && fetchedUser.city.title ? fetchedUser.city.title : ''}
-			>
-				{`${fetchedUser.first_name} ${fetchedUser.last_name}`}
-			</Cell>
-		</Group>}
+			<Group header={<Header mode="secondary">User Data Fetched with VK Bridge</Header>}>
+				<Cell
+					before={fetchedUser.photo_200 ? <Avatar src={fetchedUser.photo_200} /> : null}
+					description={fetchedUser.city && fetchedUser.city.title ? fetchedUser.city.title : ''}
+				>
+					{`${fetchedUser.first_name} ${fetchedUser.last_name}`}
+				</Cell>
+			</Group>}
 
 		<Group header={<Header mode="secondary">Navigation Example</Header>}>
 			<Div>
